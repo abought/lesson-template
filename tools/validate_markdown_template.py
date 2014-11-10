66c389dcbd1077efab898b2ebe9c173437659a1c
Andy Boughton <abought@gmail.com>
2014-11-10 02:26:52 -0500

    Add validation methods and refactor ast helpers to separate class

--- tools/validate_markdown_template.py
+++ tools/validate_markdown_template.py
@@ -1,30 +1,36 @@
 #! /usr/bin/env python
 
-import argparse, logging, sys
+import argparse, json, logging, sys
 import CommonMark
 
 
-class MarkdownValidator(object):
-    """Base class for markdown validation; contains helper methods for working with the CommonMark ast,
-    and basic validation skeleton to be extended for specific page types"""
-    HEADINGS = []  # Tuples of (heading_text, count). Use None if there is no limit on how many times the heading can appear.
+#### Common validation functions ###
+def is_list(text):
+    """Validate whether the provided string can be converted to a valid python list"""
+    text = text.strip()
+    try:
+        text_as_list = json.loads(text)
+    except:
+        return False
 
-    def __init__(self, filename):
-        with open(filename, 'rU') as f:
-            self.markdown = f.read()
+    return isinstance(text_as_list, list)
 
-        self.ast = self._parse_markdown()
 
-    def _parse_markdown(self):
-        parser = CommonMark.DocParser()
-        ast = parser.parse(self.markdown)
-        return ast
+def is_str(text):
+    """Validate whether the input is a non-blank python string"""
+    return isinstance(text, basestring) and len(text) > 0
 
-    ### Helper methods for working with CommonMark ast
-    def _has_section_heading(self, section_title, ast_node=None, limit=None):
-        """Does the markdown contain (no more than x copies of) the specified heading?"""
+
+class CommonMarkHelper(object):
+    """Basic helper functions for working with the internal abstract syntax tree produced by CommonMark parser"""
+    def __init__(self, ast):
+        self.data = ast
+        self.children = self.data.children
+
+    def has_section_heading(self, section_title, ast_node=None, limit=None):
+        """Does the markdown contain (no more than x copies of) the specified heading text?"""
         if ast_node is None:
-            ast_node = self.ast
+            ast_node = self.data  # If no level of the document is specified, search top-level headings
         if limit is None:
             limit = sys.maxint
 
@@ -32,20 +38,17 @@ class MarkdownValidator(object):
         num_nodes = len(nodes)
         if num_nodes == 0:
             logging.error("Document does not contain the specified heading: {0}".format(section_title))
-            print 1
         elif num_nodes > limit:
             logging.error("Document must not contain more than {0} copies of the heading {1}".format(limit, section_title))
-            print 2
         else:
             logging.info("Verified that document contains the specified heading: {0}".format(section_title))
-            print 3
         return (0 < num_nodes <= limit)
 
-    def _get_child_type(self, ast_node, child_type):
+    def get_child_type(self, ast_node, child_type):
         """Get all children of a given node, if they are of the specified type"""
         return [n for n in ast_node.children if n.t == child_type]
 
-    def _has_number_children(self, ast_node, exact=None, minc=0, maxc=sys.maxint):
+    def has_number_children(self, ast_node, exact=None, minc=0, maxc=sys.maxint):
         """Does the specified node (such as a bulleted list) have the expected number of children?"""
 
         if exact:  # If specified, must have exactly this number of children
@@ -53,16 +56,83 @@ class MarkdownValidator(object):
 
         return (minc <= len(ast_node.children) <= maxc)
 
-    # Perform validation. Override in child classes with specific validators.
+    def is_hr(self, ast_node):
+        """Is the node a horizontal rule (hr)?"""
+        return ast_node.t == 'HorizontalRule'
+
+    def is_heading(self, ast_node):
+        """Is the node a heading/ title?"""
+        return ast_node.t == "ATXHeader"
+
+    def is_paragraph(self, ast_node):
+        """Is the node a paragraph?"""
+        return ast_node.t == "Paragraph"
+
+
+class MarkdownValidator(object):
+    """Base class for markdown validation; contains helper methods for working with the CommonMark ast,
+    and basic validation skeleton to be extended for specific page types"""
+    # TODO: Change headings to dict
+    HEADINGS = []  # Tuples of (heading_text, count). Use None if there is no limit on how many times the heading can appear.
+    DOC_HEADERS = {}
+
+
+    def __init__(self, filename):
+        with open(filename, 'rU') as f:
+            self.markdown = f.read()
+
+        ast = self._parse_markdown()
+        self.ast = CommonMarkHelper(ast)
+
+    def _parse_markdown(self):
+        parser = CommonMark.DocParser()
+        ast = parser.parse(self.markdown)
+        return ast
+
+    def __validate_hrs(self):
+        """Verify that the header section at top of document is bracketed by two horizontal rules"""
+        # The header section should be bracketed by two HRs in the markup
+        valid = True
+        for hr in [self.ast.children[0], self.ast.children[2]]:
+            if not self.ast.is_hr(hr):
+                logging.error("Expected horizontal rule (---) at line: {0}".format(hr.start_line))
+                valid = False
+        return valid
+
+    def __validate_one_doc_header_row(self, text):
+        """Validate a single row of the document header section"""
+        label, content = text.split(":", 1)
+        if label not in self.DOC_HEADERS:
+            logging.warning("Unrecognized label in document header section: {0}".format(label))
+            return False
+
+        validate_header = self.DOC_HEADERS[label]
+        if not validate_header:
+            logging.error("Document header for label {0} does not follow expected format".format(label))
+        return validate_header
+
+    # Methods related to specific validation. Override in child classes with specific validators.
+    def _validate_doc_headers(self):
+        """Validate that the header of the document contains the specified sections with the expected contents
+            Fail if any extraneous headers are present"""
+        # Header section wrapped in hrs
+        has_hrs = self.__validate_hrs()
+
+        # Labeled sections in the actual headers should match expected format
+        ast_header_node = self.ast.children[1]
+        checked_headers = [self.__validate_one_doc_header_row(s)
+                           for s in ast_header_node.strings]
+
+        # No extraneous headers
+        only_headers = (len(ast_header_node.strings) == len(self.DOC_HEADERS.keys()))
+
+        return has_hrs and all(checked_headers) and only_headers
+
     def _validate_section_headings(self):
         """Verify that the top level document contains all specified headings. (don't just stop when one fails)"""
-        tests = [self._has_section_heading(heading_text, limit=max_count)
+        tests = [self.ast.has_section_heading(heading_text, limit=max_count)
                  for heading_text, max_count in self.HEADINGS]
         return all(tests)
-
-    def _validate_doc_headers(self):
-        """Validate that the header of the document contains the specified sections with the expected contents"""
-        return True
 
     def validate(self):
         """Perform all required validations"""
@@ -75,19 +145,21 @@ class HomePageValidator(MarkdownValidator):
     """Validate the contents of the homepage (index.md)"""
     HEADINGS = [('Topics', 1),
                 ('Other Resources', 1)]
-    # TODO: Prerequisites should be a subheading under the paragraph of introductory material section. How to handle?
-    # ('Prerequisites', 1),
+
+    DOC_HEADERS = {'layout': is_str,
+                   'title': is_str,
+                   'keywords': is_list}
 
     def _validate_intro_section(self):
         """Validate the content of a paragraph / intro section"""
         intro_section = self.ast.children[3]
-        print vars(intro_section)
         # FIXME: Prereqs is inside a block element
-        return self._has_section_heading("Prerequisites", ast_node=intro_section, limit=1)
+        return self.ast.has_section_heading("Prerequisites", ast_node=intro_section, limit=1)
 
     def validate(self):
         tests = [self._validate_intro_section()]
-        return all(tests) and super(HomePageValidator, self).validate()
+        parent_tests = super(HomePageValidator, self).validate()
+        return all(tests) and parent_tests
 
 
 class TopicPageValidator(MarkdownValidator):
