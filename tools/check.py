957d52b95df3825f6d98cdb7b5d9b5bf70083c1a
Andy Boughton <abought@gmail.com>
2015-02-06 16:53:54 -0500

    Manually restore line missing in gh-pages; bring to sync with core

--- tools/check.py
+++ tools/check.py
@@ -537,7 +537,8 @@ class ReferencePageValidator(MarkdownValidator):
         ```definition_lists``` extension.
 
         That syntax isn't supported by the CommonMark parser, so we identify
-         terms manually."""
+        terms manually."""
+        glossary_keyword = glossary_entry[0]
         if len(glossary_entry) < 2:
             logging.error(
                 "In {0}: "
